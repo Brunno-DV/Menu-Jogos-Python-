9a726a26e709f787db65430c3d489a60408667c4
Brunno-DV <bruno.eletronic100@gmail.com>
2022-09-16 07:58:07 -0300

    Fechando parte 4 do curso inicial de Python

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ print("Olá, bem vindo ao jogo de advinhação")
 print("************************************")
 
 num_sec = 42
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu chute: ")
+while(rodada <= total_tentativas):
+  print("tentativas: {} de {}" .format(rodada, total_tentativas))
+  
+  chute_str = input("Digite o seu chute: ")
+  print("Você digitou: ", chute_str)
+  chute = int(chute_str)
+  
+  acertou = num_sec == chute
+  maior   = num_sec > chute
+  menor   = num_sec < chute
 
-print("Você digitou: ", chute_str)
+  if(acertou):
+    print("Você acertou!!!")
+  else:
+    if (maior):
+      print("Você errou... Seu chute foi menor")
+    elif (menor):
+      print("Você errou... Seu chute foi maior")
 
-chute = int(chute_str)
-
-if(num_sec == chute):
-  print("Você acertou!!!")
-else:
-  print("Você errou...")
-
+  rodada = rodada + 1  
+  
 print("Fim do jogo")
